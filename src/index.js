16a92ea901ac3545f62be0a5561b931f67bae311
unknown <Bambibi_1994@yahoo.com.vn>
2022-05-16 22:05:02 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
+
+// import StateCounter from './components/StateCounter';
+// // import {connect } from 'react-redux';
+
+// ReactDOM.render(<StateCounter />, document.getElementById('root'));
+
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReduxCounter from './components/ReduxCounter';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = { count: 0 };
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1,
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1,
+            };
+            default:
+                return state;
+    }
+}
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ReduxCounter />
+    </Provider>,
+    document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
